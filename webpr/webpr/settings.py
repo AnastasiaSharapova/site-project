67eb093e848f64433717879ef3b7473a3931b61f
AnastasiaSharapova <105391066+AnastasiaSharapova@users.noreply.github.com>
2022-08-08 18:13:31 +0300

    Update settings.py

--- webpr/webpr/settings.py
+++ webpr/webpr/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5g9=lip*h*5&-v#qxml*-)!_mun=0^teg5tms)^e@fboyds4nt'
+SECRET_KEY = 'SECRETKEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
